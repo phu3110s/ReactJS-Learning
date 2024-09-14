0cd4450ef86b610321ce357181a4f4566593c85f
Phu3110 <phumse180166@fpt.edu.vn>
2024-09-14 23:42:55 +0700

    Update text color

--- src/App.js
+++ src/App.js
@@ -5,21 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <img src={logo} className="App-logo" alt="logo" />
+       <h2>Hello</h2> 
+       <h1 style={{ color : 'red' }}>
+          This is react!
+       </h1>    
       </header>
     </div>
   );
 }
-
 export default App;
